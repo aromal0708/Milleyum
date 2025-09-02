ea82bfb719eeea48b7d109ff14f2f28380646df3
Aromal0708 <aromalsunil12@gmail.com>
2025-09-02 10:19:28 +0530

    feat:landing page changes

--- milleyum-storefront/src/app/[countryCode]/(main)/page.tsx
+++ milleyum-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,42 +1,86 @@
 import { Metadata } from "next"
-import Hero from "@modules/home/components/hero"
-import { listCollections } from "@lib/data/collections"
-import { getRegion } from "@lib/data/regions"
-import About from "@modules/home/components/about"
+
+import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
+import StoreTemplate from "@modules/store/templates"
+import StoreHero from "@modules/home/components/store-hero"
 import Products from "@modules/home/components/products"
-import Features from "@modules/home/components/features"
-import Footer from "@modules/layout/templates/footer"
+import BuyProducts from "@modules/store/components/buy-products"
+import MadeForYou from "@modules/store/components/made-for-you"
+import ShopOurProducts from "@modules/store/components/shop-our-products"
+import StoreFeatures from "@modules/store/components/store-features"
+import Subscribe from "@modules/store/components/subscribe"
+import ShopFooter from "@modules/store/components/shop-footer"
+import { notFound } from "next/navigation"
+import { getRegion } from "@lib/data/regions"
+import { listProducts } from "@lib/data/products"
+import { Product } from "@medusajs/js-sdk/dist/admin/product"
+import { StoreProduct } from "@medusajs/types"
 
 export const metadata: Metadata = {
-  title: "Melliyum",
-  description:
-    "Milleyum offers high-protein, no-added-sugar millet-based beverages with probiotic benefits. Perfect for health-conscious urban professionals.",
+  title: "Store",
+  description: "Explore all of our products.",
 }
 
-export default async function Home(props: {
-  params: Promise<{ countryCode: string }>
-}) {
+type Params = {
+  searchParams: Promise<{
+    sortBy?: SortOptions
+    page?: string
+  }>
+  params: Promise<{
+    countryCode: string
+  }>
+}
+
+export type SimplifiedProducts = {
+  id: string
+  title: string
+  description?: string
+  price: number
+  thumbnail: string | null
+}
+
+export default async function StorePage(props: Params) {
   const params = await props.params
+  const region = await getRegion(params.countryCode)
 
-  const { countryCode } = params
+  if (!region) {
+    notFound()
+  }
 
-  const region = await getRegion(countryCode)
-
-  const { collections } = await listCollections({
-    fields: "id, handle, title",
+  const response = await listProducts({
+    countryCode: params.countryCode,
+    queryParams: {
+      limit: 12,
+    } as any,
   })
 
-  if (!collections || !region) {
-    return null
+  const products: StoreProduct[] = response?.response.products || []
+
+  const simplifiedProducts: SimplifiedProducts[] = products.map((product) => ({
+    id: product.id,
+    title: product.title,
+    description: product.description ?? "",
+    price: product.variants?.[0]?.calculated_price?.calculated_amount ?? 0,
+    thumbnail: product.thumbnail,
+  }))
+
+  console.log(simplifiedProducts)
+
+  if (!response || !products || products.length === 0) {
+    notFound()
   }
 
   return (
-    <div className="container px-4 sm:px-6 md:px-8 lg:px-14">
-      <Hero />
-      <About />
-      <Products />
-      <Features />
-      <Footer />
+    <div className="w-full">
+      <div className="flex flex-col items-center justify-center w-full px-4 sm:px-6 md:px-8 lg:px-14">
+        <StoreHero />
+        <BuyProducts products={simplifiedProducts} />
+        <MadeForYou products={simplifiedProducts} />
+        <ShopOurProducts products={simplifiedProducts} />
+        <StoreFeatures />
+        <Subscribe />
+      </div>
+      <ShopFooter />
     </div>
   )
 }
